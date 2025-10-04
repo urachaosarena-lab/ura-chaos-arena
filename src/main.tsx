977eda4ddd2b3a76622e4eeb3eca8fc292600295
urachaosarena-lab <urachaos.arena@gmail.com>
2025-10-04 03:45:59 +0200

    Fix App import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import { WalletContextProvider } from './wallet/WalletContextProvider'
-import App from './ui/App'
+import { App } from './ui/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
